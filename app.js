db24b29eff1252eacbdc67944c268da3a67d7441
AliciaJz <zilaphono@gmail.com>
2018-02-06 19:05:17 -0600

    mas screens

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 var images = {
+  name: "",
   bg: "images/background.png",
-  ufo: "images/UFOCostume_2.png",
-  planets:"images/drawn-planets-transparent-pixel-18SM.png",
-  start:"",
-  onePlayer:"",
-  twoPlayers:"",
-  level:"",
-  normal:"",
-  med:"",
-  high:"",
-  ready:"",
-  three:"",
-  two:"",
-  one:""
+  ufo: "images/UFOCostume.png",
+  planets: "images/drawn-planets-transparent-pixel-18SM.png",
+  start: "",
+  onePlayer: "",
+  twoPlayers: "",
+  level: "",
+  normal: "",
+  med: "",
+  high: "",
+  ready: "",
+  three: "",
+  two: "",
+  one: ""
 };
 
 var board;
@@ -41,10 +42,10 @@ function Board() {
 }
 
 function Ufo() {
-  this.x = 0;
-  this.y = 0;
+  this.x = 100;
+  this.y = 400;
   this.width = 100;
-  this.height = 85;
+  this.height = 90;
   this.img = new Image();
   this.img.src = images.ufo;
   this.img.onload = function() {
@@ -55,34 +56,46 @@ function Ufo() {
   };
 }
 
-function Planet(){
-    this.x = 0;
-    this.y = 0;
-    this.width = 85;
-    this.height = 85;
-    this.img = new Image();
-    this.img.src = images.planets;
-    this.img.onload = function() {
-        this.draw();
+function Planet() {
+  this.x = 0;
+  this.y = 0;
+  this.width = 85;
+  this.height = 85;
+  this.img = new Image();
+  this.img.src = images.planets;
+  this.img.onload = function() {
+    this.draw();
   }.bind(this);
   this.draw = function() {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   };
 }
 
-function startGame() {
-  frames = 0;
-  planets = [new Planet(100,100)];
+function firstScreen() {
   board = new Board();
-  ufo = new Ufo();
-  interval = setInterval(updateGame,1000/60);
 }
 
-function updateGame(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    board.draw();
-    ufo.draw();
-    drawPlanets();
+function playersScreen() {
+  board = new Board();
+}
+
+function levelScreen() {
+  board = new Board();
+}
+
+function startGame() {
+  frames = 0;
+  //   planets = [new Planet(100,100)];
+  board = new Board();
+  ufo = new Ufo();
+  interval = setInterval(updateGame, 1000 / 60);
+}
+
+function updateGame() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  board.draw();
+  ufo.draw();
+  // drawPlanets();
 }
 
 function stopGame() {
@@ -90,17 +103,16 @@ function stopGame() {
   ctx.fillText("GAME OVER", 200, 100);
 }
 
-function drawPlanets(){
-    planets.forEach(function(planet){
-        planet.draw()
-    })
+function drawPlanets() {
+  planets.forEach(function(planet) {
+    planet.draw();
+  });
 }
 
 addEventListener("mousemove", function(e) {
-    ufo.x = e.clientX-60;
-    ufo.y = e.clientY-60; 
-    // console.log("coordenadas", e.clientX, e.clientY)
-
+  ufo.x = e.clientX - 60;
+  ufo.y = e.clientY - 60;
+  // console.log("coordenadas", e.clientX, e.clientY)
 });
 
-startGame();
+firstScreen();
